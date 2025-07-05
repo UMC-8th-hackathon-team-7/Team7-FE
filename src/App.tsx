1fc209b2335ebeff9d1544c91fe95248b3a1c1c5
Junhyeok <cjh64866@naver.com>
2025-07-05 16:50:57 +0900

    chore: 색상 variable 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <DefaultLayout />,
-    children: [],
+    children: [
+      {
+        path: "/",
+        element: (
+          <div className="text-[var(--color-fill-inverted)]">
+            faasdffasdadsf
+          </div>
+        ),
+      },
+    ],
   },
 ]);
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,27 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+import { defineConfig } from "tailwindcss";
+
+export default defineConfig({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        suit: ['SUIT', 'sans-serif'], // 커스텀 폰트 등록
+        suit: ["SUIT", "sans-serif"],
+      },
+      colors: {
+        "fill-regular": "var(--color-fill-regular)",
+        "fill-strong": "var(--color-fill-strong)",
+        "fill-static": "var(--color-fill-static)",
+        "fill-interactive": "var(--color-fill-interactive)",
+        "fill-inverted": "var(--color-fill-inverted)",
+        base: "var(--color-base)",
+        additive: "var(--color-additive)",
+        assistive: "var(--color-assistive)",
+        disabled: "var(--color-disabled)",
+        inverted: "var(--color-inverted)",
+        elevated: "var(--color-elevated)",
       },
     },
   },
   plugins: [],
-}
-
+});
